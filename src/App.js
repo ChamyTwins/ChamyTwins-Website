4211734c2403d563ff2e030bdb4203cf976acc57
Kristian <kristian14214@gmail.com>
2024-07-20 17:23:52 +0700

    ganti routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Admin/Login.js";
 import Homepage from "./User/Homepage.js";
 import Caresheet from "./User/Caresheet.js";
